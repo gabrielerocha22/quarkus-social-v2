2594178f000ea9fd647f2fa115b2176d0971aec7
Gabriele Rocha <56683790+gabrielerocha22@users.noreply.github.com>
2022-11-18 14:44:44 -0300

    Update PostResponse.java

--- src/main/java/com/git/gabriele/dto/PostResponse.java
+++ src/main/java/com/git/gabriele/dto/PostResponse.java
@@ -8,17 +8,7 @@ public class PostResponse {
 
 	private String text;
 	private LocalDateTime dateTime;
-<<<<<<< HEAD
-	
-	
-//	public PostResponse(String text, LocalDateTime dateTime) {
-//		this.text = text;
-//		this.dateTime = dateTime;
-//	}
-	
-=======
 
->>>>>>> 6f609a0 (add exceptions)
 	public String getText() {
 		this.text = text;
 		return text;
@@ -32,19 +22,12 @@ public class PostResponse {
 	public void setDateTime(LocalDateTime dateTime) {
 		this.dateTime = dateTime;
 	}
-<<<<<<< HEAD
-	
-=======
->>>>>>> 6f609a0 (add exceptions)
+
 	public static PostResponse fromEntity (Post post) {
 		var response = new PostResponse();
         response.setText(post.getText());
         response.setDateTime(post.getDataTime());
         return response;
         }
-<<<<<<< HEAD
-	
-	
-=======
->>>>>>> 6f609a0 (add exceptions)
+
 }
