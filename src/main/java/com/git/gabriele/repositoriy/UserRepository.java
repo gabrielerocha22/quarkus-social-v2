ba12c07c8e6119f16b7ebce0c3fca239b0c09657
Gabriele Rocha <56683790+gabrielerocha22@users.noreply.github.com>
2022-11-18 14:32:27 -0300

    Update UserRepository.java

--- src/main/java/com/git/gabriele/repositoriy/UserRepository.java
+++ src/main/java/com/git/gabriele/repositoriy/UserRepository.java
@@ -1,18 +1,14 @@
 package com.git.gabriele.repositoriy;
 
 import javax.enterprise.context.ApplicationScoped;
-<<<<<<< HEAD
 
 import com.git.gabriele.model.User;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
-
-=======
 import com.git.gabriele.model.User;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
->>>>>>> 6f609a0 (add exceptions)
 @ApplicationScoped
 public class UserRepository implements PanacheRepository<User>{
 	
