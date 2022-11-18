b58ee4bd5d2e8b811383e8ad930011af7663c460
Gabriele Rocha <56683790+gabrielerocha22@users.noreply.github.com>
2022-11-18 14:46:03 -0300

    Update ResponseError.java

--- src/main/java/com/git/gabriele/dto/ResponseError.java
+++ src/main/java/com/git/gabriele/dto/ResponseError.java
@@ -12,26 +12,14 @@ public class ResponseError {
 	
 	public static final int UNPROCESSABLE_ENTITY_STATUS = 422;
 
-<<<<<<< HEAD
-	
 	private String message;
 	private Collection<FieldError>errors;
-	
 
-=======
-	private String message;
-	private Collection<FieldError>errors;
-	
->>>>>>> 6f609a0 (add exceptions)
 	public ResponseError(String message, Collection<FieldError> errors) {
 		this.message = message;
 		this.errors = errors;
 	}
-	
-<<<<<<< HEAD
-	
-=======
->>>>>>> 6f609a0 (add exceptions)
+
 	public static <T> ResponseError createFromValidation(Set<ConstraintViolation<T>> violations) {
 		List<FieldError> errors = violations.stream().map(cv -> new FieldError(cv.getPropertyPath().toString(), cv.getMessage())).collect(Collectors.toList());
 		
@@ -51,16 +39,8 @@ public class ResponseError {
 	public void setErrors(Collection<FieldError> errors) {
 		this.errors = errors;
 	}
-<<<<<<< HEAD
-	
+
 	public Response withStatusCode(int code) {
 		return Response.status(code).entity(this).build();
 	}
-	
-	
-=======
-	public Response withStatusCode(int code) {
-		return Response.status(code).entity(this).build();
-	}
->>>>>>> 6f609a0 (add exceptions)
 }
